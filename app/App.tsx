658850a7fd2aee2842fe67140130605b654f483f
gene <pocciaeugenio@gmail.com>
2019-07-12 11:23:33 -0400

    front end fix in progress

--- app/App.tsx
+++ app/App.tsx
@@ -1,47 +1,49 @@
-import React, {Component} from "react";
-import {connect} from "react-redux";
-import {Link, Route} from "react-router-dom";
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { Link, Route } from "react-router-dom";
 
 interface State {
-    planters: object[];
+  planters: object[];
 }
 
 class UnconnectedApp extends Component<any> {
-    constructor(props: object) {
-        super(props);
-    }
-    public componentDidMount = () => {
-        fetch("http://localhost:4000/planter")
-          .then((x) => {
-            return x.text();
-          })
-          .then((responseBody) => {
-            const body = JSON.parse(responseBody);
-            this.props.dispatch({
-              type: "load-planters",
-              planters: body.results,
-            });
-          });
-    }
+  constructor(props: object) {
+    super(props);
+  }
+  public componentDidMount = () => {
+    fetch("http://localhost:4000/planter")
+      .then(x => {
+        return x.text();
+      })
+      .then(responseBody => {
+        const body = JSON.parse(responseBody);
+        this.props.dispatch({
+          type: "load-planters",
+          planters: body.results
+        });
+      });
+  };
 
-    public renderRoot = () => {
-        return <div>hello world</div>;
-    }
+  public renderRoot = () => {
+    return <div>hello world</div>;
+  };
 
-    public renderPlanters = () => {
-        return <div> planters go here </div>;
-    }
+  public renderPlanters = () => {
+    return <div> planters go here </div>;
+  };
 
-    public render = () => {
-        return <div>
-            <Route exact={true} path = "/" render = {this.renderRoot} />
-            <Route exact={true} path = "/planters" render = {this.renderPlanters} />
-            </div>;
-    }
+  public render = () => {
+    return (
+      <div>
+        <Route exact={true} path="/" render={this.renderRoot} />
+        <Route exact={true} path="/planters" render={this.renderPlanters} />
+      </div>
+    );
+  };
 }
 
 const mapStateToProps = (state: State) => {
-    return {planters: state.planters};
+  return { planters: state.planters };
 };
 
 const App = connect(mapStateToProps)(UnconnectedApp);
